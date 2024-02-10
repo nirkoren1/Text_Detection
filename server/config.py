3848019dabac9fe95f4abc6a9b1598c8622dacbf
nirkoren1 <nirkoren1@gmail.com>
2024-02-10 14:36:31 +0200

    changed server url

--- server/config.py
+++ server/config.py
@@ -1,2 +1,2 @@
-detection_model_path = './models_weights/saved_model13.pth'
-recognition_model_path = './models_weights/saved_model9.pth'
+detection_model_path = './models_weights/saved_model (13).pth'
+recognition_model_path = './models_weights/saved_model (9).pth'
