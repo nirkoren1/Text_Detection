98a2b65581bd2bfe1a3da46ebffcfe6544b37c51
nirkoren1 <nirkoren1@gmail.com>
2024-02-10 22:51:44 +0200

    Updates

---
+++ precache-manifest.fbfb75a96c284ffa30b804278aa221d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af6f597ac023b7f244a2a10b4de900e8",
+    "url": "/Text_Detection/index.html"
+  },
+  {
+    "revision": "f1dd2079e7cd6bac320c",
+    "url": "/Text_Detection/static/css/main.a63a7212.chunk.css"
+  },
+  {
+    "revision": "8ccf274cb5b44287b446",
+    "url": "/Text_Detection/static/js/2.806183ce.chunk.js"
+  },
+  {
+    "revision": "532e0de6b626acdae4af",
+    "url": "/Text_Detection/static/js/3.e91cd1ee.chunk.js"
+  },
+  {
+    "revision": "f1dd2079e7cd6bac320c",
+    "url": "/Text_Detection/static/js/main.ba387e69.chunk.js"
+  },
+  {
+    "revision": "d1f73b6bb244de328e1f",
+    "url": "/Text_Detection/static/js/runtime~main.24f9ba53.js"
+  }
+]);
