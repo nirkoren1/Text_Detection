25ff05e25a78d8f7e2d181a3c9aa3f8d76fe0503
nirkoren1 <nirkoren1@gmail.com>
2024-02-12 14:11:17 +0200

    .

---
+++ client/scene_ocr/build/precache-manifest.bd79526230c059f75a0a3fb1950d2bd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45825e2b61c96ed12e52748e298bf888",
+    "url": "/Text_Detection/index.html"
+  },
+  {
+    "revision": "d99e5ffc2b4534728798",
+    "url": "/Text_Detection/static/css/main.a63a7212.chunk.css"
+  },
+  {
+    "revision": "8ccf274cb5b44287b446",
+    "url": "/Text_Detection/static/js/2.806183ce.chunk.js"
+  },
+  {
+    "revision": "532e0de6b626acdae4af",
+    "url": "/Text_Detection/static/js/3.e91cd1ee.chunk.js"
+  },
+  {
+    "revision": "d99e5ffc2b4534728798",
+    "url": "/Text_Detection/static/js/main.8eb50eef.chunk.js"
+  },
+  {
+    "revision": "d1f73b6bb244de328e1f",
+    "url": "/Text_Detection/static/js/runtime~main.24f9ba53.js"
+  }
+]);
